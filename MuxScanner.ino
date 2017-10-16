12b0fc0bf2fafbe816ecc554be75293215f3d2c5
Jessie A. Morris <jessie.morris@finicity.com>
2017-10-16 09:52:46 -0600

    Improve configuration flexibility a bit more
    Specifically this allows matrixes to be any dimension. If you have
    unused pins on a given matrix (i.e. you typically use 8x8 matrixes but
    your pedal keyboard division only needs an 8x4) you can specify the
    input pin as "0" to have it be ignored.

--- MuxScanner.ino
+++ MuxScanner.ino
@@ -1,15 +1,15 @@
-//The keyboard/piston/stops multiplexers use multiple 8x8 diode matrices to select up to 64 switch positions per matrix.
+//The keyboard/piston/stops multiplexers use multiple OUTPUT_PINS by INPUT_PINS diode matrices to select up to 64 switch positions per matrix.
 //All matrices use the same set of eight row addressing pins to select the current row to scan.
 //Each matrix has its own set of eight input lines, one for each column.
 
 void InitialiseMultiplexer() {
   currentMuxSelectRow = 0;
-  for (byte row = 0; row < 8; row++) {
+  for (byte row = 0; row < OUTPUT_PINS; row++) {
     pinMode(MuxRowAddressPins[row], OUTPUT);
   }
 
   for (int keyboard = 0; keyboard < ENABLED_KEYBOARDS; keyboard++) {
-    for (byte col = 0; col < 8; col++) {
+    for (byte col = 0; col < INPUT_PINS; col++) {
       if(KeyboardInputPins[keyboard][col] != 0) {
         pinMode(KeyboardInputPins[keyboard][col], INPUT);
       }
@@ -17,7 +17,7 @@ void InitialiseMultiplexer() {
   }
 
   for (int piston = 0; piston < EnabledPistons; piston++) {
-    for (byte col = 0; col < 8; col++) {
+    for (byte col = 0; col < INPUT_PINS; col++) {
       if(PistonInputPins[piston][col] != 0) {
         pinMode(PistonInputPins[piston][col], INPUT);
       }
@@ -26,7 +26,7 @@ void InitialiseMultiplexer() {
 }
 
 void SetRowAddressLines() {
-  for (byte row = 0; row < 8; row++) {
+  for (byte row = 0; row < OUTPUT_PINS; row++) {
     digitalWriteDirect(MuxRowAddressPins[row], LOW);
   }
   digitalWriteDirect(MuxRowAddressPins[currentMuxSelectRow], HIGH);
@@ -38,18 +38,18 @@ void ScanKeyboardMultiplexers() {
   byte col;
 
   //Set row address lines
-  for (row = 0; row < 8; row++) {
+  for (row = 0; row < OUTPUT_PINS; row++) {
     digitalWriteDirect(MuxRowAddressPins[row], LOW);
   }
   digitalWriteDirect(MuxRowAddressPins[currentMuxSelectRow], HIGH);
   delayMicroseconds(20);   //Address line settling time
 
   //Read columns for each existing keyboard
-  for (col = 0; col < 8; col++) {
+  for (col = 0; col < INPUT_PINS; col++) {
     for (byte keyboard = 0; keyboard < ENABLED_KEYBOARDS; keyboard++) {
       if(KeyboardInputPins[keyboard][col] != 0) {
         int pinValue = digitalReadDirect(KeyboardInputPins[keyboard][col]);
-        processKey(currentMuxSelectRow + col * 8, pinValue, keyboard);
+        processKey(currentMuxSelectRow + col * INPUT_PINS, pinValue, keyboard);
       }
     }
 
@@ -57,14 +57,14 @@ void ScanKeyboardMultiplexers() {
     if (PistonMatrixExists) {
       int PinValue = digitalReadDirect(PistonsRowInputPins[col]);
       //Switches 00..63 are pistons 32..95
-      ProcessPiston(currentMuxSelectRow + ((col + 4) * 8), PinValue);
+      ProcessPiston(currentMuxSelectRow + ((col + 4) * INPUT_PINS), PinValue);
     }
     */
   }
 
   //Step to next row
   currentMuxSelectRow++;
-  if (currentMuxSelectRow > 7) {
+  if (currentMuxSelectRow > (OUTPUT_PINS - 1)) {
     currentMuxSelectRow = 0;
   }
 }
--- config.h
+++ config.h
@@ -1,28 +1,31 @@
 #define PrintMIDITransactions true     //True to print MIDI transactions to programming port
 
 #define ENABLED_KEYBOARDS 1
-#define EnabledPistons 2
+#define EnabledPistons 0
 
 //Sizing definitions
+#define OUTPUT_PINS 2
+#define INPUT_PINS 3
 #define MaxKeys 64
 #define MaxPistons 96
 
 const String KeyboardName[ENABLED_KEYBOARDS] = { "Swell" };
 
 //Keyboard multiplexer row addressing output pins
-const byte MuxRowAddressPins[8] = { 2, 3, 4, 5, 6, 7, 8, 9 };
+const byte MuxRowAddressPins[OUTPUT_PINS] = { 9, 10 };
 
 //Keyboard & piston multiplexer column input pins
-const byte KeyboardInputPins[ENABLED_KEYBOARDS][8] = {
-  { 22, 23, 0, 0, 0, 0, 0, 0        },     //Swell notes 00..63
+const byte KeyboardInputPins[ENABLED_KEYBOARDS][INPUT_PINS] = {
+  {22, 23, 24},
+  //{ 22, 23, 24, 25, 26, 27, 28, 29 },     //Swell notes 00..63
   //{ 30, 31, 32, 33, 34, 35, 36, 37 },   //Great notes 00..63
   //{ 38, 39, 40, 41, 42, 43, 44, 45 },   //Choir notes 00..63
-  //{ 46, 47, 48, 49, 0, 0, 0, 0 }    // Pedals 00..31
+  //{ 46, 47, 48, 49,  0,  0,  0,  0 }    // Pedals 00..31
 };
 
-const byte PistonInputPins[EnabledPistons][8] = {
-  { 50, 51, 52, 53, 0, 0, 0, 0 },  //Pistons/stops 00..31
-  { 14, 15, 16, 17, 18, 19, 20, 21 },  //Pistons/stops 32..95
+const byte PistonInputPins[EnabledPistons][INPUT_PINS] = {
+  //{ 50, 51, 52, 53,  0,  0,  0,  0 },  //Pistons/stops 00..31
+  //{ 14, 15, 16, 17, 18, 19, 20, 21 },  //Pistons/stops 32..95
 };
 
 //Swell pedal input pins
